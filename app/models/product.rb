e0a99de154c3bede57d50e5df830c267de45601c
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-06-03 11:49:28 -0700

    add validations to product.rb to check for blank, max length, and titlelizing first letter

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :name, presence: true 
+  validates_length_of :name, maximum: 100 
+
+  before_save(:titlelize_product)
+
+  private 
+    def titlelize_product
+      self.name = self.name.titleize
+    end
 end

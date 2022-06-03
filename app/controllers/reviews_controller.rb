da35aed3cad9be4c726cd6517bc84c312be5d39a
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-06-03 00:54:00 -0700

    add methods edit, update, destroy and private to review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -23,4 +23,31 @@ class ReviewsController < ApplicationController
     render :show
   end
 
-  
+  def edit 
+    @product = Product..find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :edit
+  end
+
+  def update 
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      flash[:notice] = "Review successfully updated"
+    else
+      @product = Product.find(params[:product_id])
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    flash[:notice] = "Review successfully deleted"
+    redirect_to product_path(@review.product)
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:author, :content_body, :rating)
+    end
+end

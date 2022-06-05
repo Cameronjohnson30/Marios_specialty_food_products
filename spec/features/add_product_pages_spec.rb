f897765af25bcb6b0c2d44ca0824c7d50dc1d98c
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-06-04 21:59:34 -0700

    add features folder to spec folder with add_product_pages

---
+++ spec/features/add_product_pages_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "the add a product process" do
+  it "adds a new product" do
+    visit products_path
+    click_link 'Add new product'
+    fill_in 'Name', :with => 'Giant Steps'
+    fill_in 'product_country_of_orgin', :with => 'USA'
+    fill_in 'Cost', :with => 5
+    click_on 'Create Product'
+    expect(page).to have_content 'Products'
+  end
+
+  it "gives an error when no name is entered" do
+    visit new_product_path
+    click_on 'Create Product'
+    expect(page).to have_content "Name can't be blank"
+  end
+end

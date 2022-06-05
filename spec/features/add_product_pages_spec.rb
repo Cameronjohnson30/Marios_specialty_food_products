dc35981de3506a15988414606688cb69525dfe62
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-06-04 23:06:13 -0700

    add before testing with create, empty, and edit tests

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -1,19 +1,29 @@
 require 'rails_helper'
 
-describe "the add a product process" do
-  it "adds a new product" do
-    visit products_path
-    click_link 'Add new product'
-    fill_in 'Name', :with => 'Giant Steps'
-    fill_in 'product_country_of_orgin', :with => 'USA'
-    fill_in 'Cost', :with => 5
-    click_on 'Create Product'
-    expect(page).to have_content 'Products'
-  end
+  describe "the add a product process" do
+  before :each do
+      visit products_path
+      click_link 'Add new product'
+      fill_in 'Name', :with => 'Giant Steps'
+      fill_in 'product_country_of_orgin', :with => 'USA'
+      fill_in 'Cost', :with => 5
+      click_on 'Create Product'
+      expect(page).to have_content 'Products'
+    end
 
-  it "gives an error when no name is entered" do
-    visit new_product_path
-    click_on 'Create Product'
-    expect(page).to have_content "Name can't be blank"
+    it "gives an error when no name is entered" do
+      visit new_product_path
+      click_on 'Create Product'
+      expect(page).to have_content "Name can't be blank"
+    end
+
+    it "edits a projects name orgin and price" do
+      click_on 'Giant Steps'
+      click_on 'Edit'
+      fill_in 'Name', :with => 'Giant'
+      fill_in 'product_country_of_orgin', :with => 'USS'
+      fill_in 'Cost', :with => 666
+      click_on 'Update Product'
+      expect(page).to have_content "Giant"
   end
 end

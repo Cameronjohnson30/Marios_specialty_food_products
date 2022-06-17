3840ee8c127aac5bc621c1bab324dd4776729be9
cameronJohnson30 <cameron_johnson91@icloud.com>
2022-06-17 14:14:40 -0700

    add titleize test to product_spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,9 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_orgin }
+ 
+  it("titleizes the name of a product") do
+    product = Product.create({name: "bread", cost: "100", country_of_orgin: "united states of america"})
+    expect(product.name()).to(eq("Bread"))
+  end
 end
